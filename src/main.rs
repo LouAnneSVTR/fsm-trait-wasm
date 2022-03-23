87e38a1c01c06c583818a12e71dedcde47c6f197
E185574L <lou-anne.sauvetre@etu.univ-nantes.fr>
2022-03-23 14:56:18 +0100

    add good proxy for trait with wasm

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+//use fsm_trait_wasm::{FSM, SimpleFiniteStateMachine};
+
+fn main() {
+
+    /*let mut test = SimpleFiniteStateMachine::new("Test");
+    let name = 0;
+    let name2 = 1;
+    let name3 = 2;
+
+    test.addNode(name);
+    test.addNode(name2);
+    test.addNode(name3);
+
+    test.addTransition('a',0,1);
+    test.addTransition('b',0,2);
+    test.addTransition('c',1,2);
+    test.addTransition('d',2,2);
+
+    test.displayFSM();
+
+    let word1 = "acd";
+    let mut result = test.process_fsm(0,word1);
+    let lenght = result.len();
+    print!("Taille du chemin pour ce mot.");
+    print!("{}",lenght);
+    dbg!(result);*/
+}
